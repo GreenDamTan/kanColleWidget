8fb311e8988462e8d82d545e05a2f35a12b06491
otiai10 <otiai10@gmail.com>
2013-12-03 16:23:59 +0900

    Create context build methods

--- src/js/definitions/i18n/i18n.js
+++ src/js/definitions/i18n/i18n.js
@@ -8,6 +8,11 @@ var KanColleWidget = KanColleWidget || {};
             // get text
             key = "test"//TMP: hard coding
             return KanColleWidget.Resource[lang][key];
+        },
+        c : function(key, params) {
+            var lang = Config.get("user-lang");
+            var module = new KanColleWidget.Resource.module(lang);
+            return module[key](params);
         }
     };
 })();
