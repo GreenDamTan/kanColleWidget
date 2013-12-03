06de6530f5a2bfee75b402c7262d03eb987fd4a1
otiai10 <otiai10@gmail.com>
2013-12-03 13:24:59 +0900

    Add 'ar'

--- src/js/definitions/i18n/resource.js
+++ src/js/definitions/i18n/resource.js
@@ -3,10 +3,13 @@ var KanColleWidget = KanColleWidget || {};
     // key-value text storage
     var Resource = KanColleWidget.Resource = {
         "ja": {
-            "test" : "！すでのなルバーログ"
+            "test": "！すでのなルバーログ"
         },
         "en": {
-            "test" : "That's what Internationalization is!!"
+            "test": "That's what Internationalization is!!"
+        },
+        "ar": {
+            "test": "هذا هو التدويل"
         }
     };
 })();
